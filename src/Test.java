26e613362bdc945a7be33e9b81e8f59d0a737460
ACER <ACER@ACER-PC>
2023-04-19 16:30:34 +0530

    First commit....

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=18;
+		
+		if(n>=18) {
+			System.out.println("ELIGIBLE TO VOTE");
+		}
+		else {
+		System.out.println("Not eligiblr to vote");
+	}
+
+}
+}
